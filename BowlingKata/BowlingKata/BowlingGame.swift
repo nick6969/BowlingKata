0a294b1c7c75ae640b3854fc6a00bc67a41369c2
Nick Lin <hawhaw.ya@gmail.com>
2018-07-21 07:19:03 +0800

    Refactor 計算分數的 Method, 新增 HitRounds 資料格式

--- BowlingKata/BowlingKata/BowlingGame.swift
+++ BowlingKata/BowlingKata/BowlingGame.swift
@@ -8,29 +8,84 @@
 
 import Foundation
 
+/// 計算分數時使用的 Error Type
+///
+/// - outsideOfRulesWithSingleHitPin: 單次擊球數量超過規則的範圍
+/// - outsideOfRulesWithSingleRounds: 單個 rounds 擊球數量超過規則的範圍
+enum CalculatedError: Error {
+    case outsideOfRulesWithSingleHitPin
+    case outsideOfRulesWithSingleRounds
+}
+
+private struct HitRounds {
+    private(set) var first: Int = -1
+    private(set) var secound: Int = -1
+
+    var isSpare: Bool {
+        if first != -1 && secound != -1 {
+            return first + secound == 10
+        }
+        return false
+    }
+
+    var score: Int {
+        if first != -1 && secound != -1 {
+            return first + secound
+        } else if first != -1 && secound == -1 {
+            return first
+        }
+        return 0
+    }
+
+    /// 填入擊中球瓶
+    ///
+    /// - Parameter pin: 擊中的球瓶數
+    /// - Returns: 是否要進入下一個 rounds
+    /// - Throws: 擊中球瓶數量錯誤的時候會給 CalculatedError
+    mutating func hitNumber(of pin: Int) throws -> Bool {
+        if first == -1 {
+            self.first = pin
+        } else {
+            self.secound = pin
+            if self.first + self.secound > 10 {
+                throw CalculatedError.outsideOfRulesWithSingleRounds
+            }
+            return true
+        }
+        return false
+    }
+
+}
+
 class BowlingGame {
 
-    private var hitPins: [Int] = []
+    // A game of ten-pin bowling is divided into ten rounds
+    private var rounds: Int = 0
+
+    private lazy var hitRoundsArray: [HitRounds] = Array(repeating: HitRounds(), count: 10)
 
     func hitNumber(of pin: Int) throws {
         if pin > 10 || pin < 0 {
             throw CalculatedError.outsideOfRulesWithSingleHitPin
         }
-        self.hitPins.append(pin)
-    }
-
-    func calculatedScore() throws -> Int {
-        let score: Int = hitPins.reduce(0, +)
-        if score > 10 {
-            throw CalculatedError.outsideOfRulesWithTwiceHitPin
-        } else {
-            return score
+        let nextRounds = try self.hitRoundsArray[rounds].hitNumber(of: pin)
+        if nextRounds {
+            rounds += 1
         }
     }
 
-}
+    func calculatedScore() -> Int {
+        var isSpare: Bool = false
+        var score: Int = 0
+        for i in 0 ..< rounds {
+            let round = hitRoundsArray[i]
+            if isSpare {
+                score += round.first
+            }
+            score += round.score
+            isSpare = round.isSpare
+        }
+        return score
+    }
 
-enum CalculatedError: Error {
-    case outsideOfRulesWithSingleHitPin
-    case outsideOfRulesWithTwiceHitPin
 }
