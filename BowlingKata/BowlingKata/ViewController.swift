bce348d5291a0bcb2dcc52795d32d0919cd13542
Nick Lin <hawhaw.ya@gmail.com>
2018-07-21 04:56:27 +0800

    init project

---
+++ BowlingKata/BowlingKata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BowlingKata
+//
+//  Created by Nick Lin on 2018/7/21.
+//  Copyright © 2018年 Nick Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
