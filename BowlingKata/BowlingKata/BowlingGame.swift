fb4bf8e979e87980e033025ed22214f9cd6cbb86
Nick Lin <hawhaw.ya@gmail.com>
2018-07-21 05:09:54 +0800

    建立計算分數的 Class 跟 Empty Method

---
+++ BowlingKata/BowlingKata/BowlingGame.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingGame.swift
+//  BowlingKata
+//
+//  Created by Nick Lin on 2018/7/21.
+//  Copyright © 2018年 Nick Lin. All rights reserved.
+//
+
+import Foundation
+
+class BowlingGame {
+
+    func calculatedScore() -> Int {
+        return 0
+    }
+
+}
