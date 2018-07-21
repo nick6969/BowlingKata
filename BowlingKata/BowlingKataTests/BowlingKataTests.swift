ef566e3070c1cef22c8899207a414905040b957d
Nick Lin <hawhaw.ya@gmail.com>
2018-07-21 12:56:35 +0800

    添加 維基百科上提供的計算範例 的 Test case

--- BowlingKata/BowlingKataTests/BowlingKataTests.swift
+++ BowlingKata/BowlingKataTests/BowlingKataTests.swift
@@ -249,4 +249,75 @@ class BowlingKataTests: XCTestCase {
             XCTAssert(false, "不應該出現, error: \(error)")
         }
     }
+
+    // https://en.wikipedia.org/wiki/Ten-pin_bowling
+    // 根據維基百科 計算分數的 Demo 部分 驗證是否正確
+    func testWiki01() {
+        do {
+            try game.hitNumber(of: 10)
+            try game.hitNumber(of: 3)
+            try game.hitNumber(of: 6)
+            let score: Int = game.calculatedScore()
+            XCTAssert(score == 28, "這裡分數應該是 28, 可是現在是 \(score)")
+        } catch {
+            XCTAssert(false, "不應該出現, error: \(error)")
+        }
+    }
+
+    func testWiki02() {
+        do {
+            try game.hitNumber(of: 10)
+            try game.hitNumber(of: 10)
+            try game.hitNumber(of: 9)
+            try game.hitNumber(of: 0)
+            let score: Int = game.calculatedScore()
+            XCTAssert(score == 57, "這裡分數應該是 57, 可是現在是 \(score)")
+        } catch {
+            XCTAssert(false, "不應該出現, error: \(error)")
+        }
+    }
+
+    func testWiki03() {
+        do {
+            try game.hitNumber(of: 10)
+            try game.hitNumber(of: 10)
+            try game.hitNumber(of: 10)
+            try game.hitNumber(of: 8)
+            try game.hitNumber(of: 2)
+            try game.hitNumber(of: 8)
+            try game.hitNumber(of: 1)
+            let score: Int = game.calculatedScore()
+            XCTAssert(score == 105, "這裡分數應該是 105, 可是現在是 \(score)")
+        } catch {
+            XCTAssert(false, "不應該出現, error: \(error)")
+        }
+    }
+
+    func testWiki04() {
+        do {
+            for _ in 0 ..< 5 {
+                try game.hitNumber(of: 10)
+            }
+            try game.hitNumber(of: 7)
+            try game.hitNumber(of: 2)
+            let score: Int = game.calculatedScore()
+            XCTAssert(score == 145, "這裡分數應該是 145, 可是現在是 \(score)")
+        } catch {
+            XCTAssert(false, "不應該出現, error: \(error)")
+        }
+    }
+
+    func testWiki05() {
+        do {
+            try game.hitNumber(of: 7)
+            try game.hitNumber(of: 3)
+            try game.hitNumber(of: 4)
+            try game.hitNumber(of: 2)
+            let score: Int = game.calculatedScore()
+            XCTAssert(score == 20, "這裡分數應該是 20, 可是現在是 \(score)")
+        } catch {
+            XCTAssert(false, "不應該出現, error: \(error)")
+        }
+    }
+
 }
